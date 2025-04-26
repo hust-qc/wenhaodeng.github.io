f0734a6c7b1119f98ade32e9a34fdcce1385adfa
Wenhao Deng <68089504+hust-qc@users.noreply.github.com>
2025-04-26 12:21:22 +0100

    Update and rename scripts_rmsd to scripts_rmsd.py

---
+++ mdanalysis/gromacs_traj_analysis/scripts_rmsd.py
@@ -0,0 +1,8 @@
+import MDAnalysis as mda
+import matplotlib.pyplot as plt
+import numpy as np
+from MDAnalysis.analysis import rms
+
+# 设置全局字体为Arial
+plt.rcParams['font.family'] = 'Arial'
+plt.rcParams['font.sans-serif'] = ['Arial']
